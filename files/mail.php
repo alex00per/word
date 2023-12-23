eb99e73dabcee2c51d11f13621a4b366b4c0f7fc
alex00per <154701933+alex00per@users.noreply.github.com>
2023-12-23 21:31:52 +0200

    first

---
+++ files/mail.php
@@ -0,0 +1,71 @@
+<?php
+
+// Declare variables
+
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'pererodovsasha2362000@gmail.com';                     //SMTP username
+    $mail->Password   = 'qufautwqjukwunxx';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress('pererodovsasha2362000@gmail.com');     //Add a recipient
+
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+
+    $body = '<h1>Встречайте супер письмо!</h1>';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['age']))){
+		$body.='<p><strong>Number:</strong> '.$_POST['phone'].'</p>';
+	}
+	
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	}
+
+    $mail->Body  = $body;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
